7f3d399fe5a0c52cf6989449a63f7e9a34c8a06e
KhiemLuc <41050460+luckhiem@users.noreply.github.com>
2020-07-27 17:24:46 +0700

    update

--- src/test_scripts/test.test.js
+++ src/test_scripts/test.test.js
@@ -34,36 +34,6 @@ describe('Adding People', () => {
         page.MethodSelect.selectMethod()
         page.PersonalDetails.addPersonalDetails(user)
         page.BusinessDetails.addBusinessDetails(business)
-        page.IdentifyDetails.addIdentify()
-        // browser.url("https://qa-test.customer-frontend.staging.aspireapp.com/sg/kyc-dashboard/identify-detail");
-        // browser.pause(3000);
-        // $('input[data-cy="register-person-phone"]').addValue(44444444);
-        // $('.aspire-button--cta').click();
-        // $('input[data-cy="digit-input-pin"]').addValue(123456);
-        // $('button[data-cy="verify-otp-submit"]').click();
-        // browser.pause(3000);
-        // browser.url("https://qa-test.customer-frontend.staging.aspireapp.com/sg/kyc-dashboard/identify-detail");
-        // browser.pause(3000);
-        // $('.aspire-button--cta').click();
-        // browser.pause(3000);
-        // $('.aspire-button--cta').click();
-        // browser.pause(3000);
-        // $(".onfido-sdk-ui-DocumentSelector-icon-national-identity-card").click();
-        // browser.pause(3000);
-        // $(".onfido-sdk-ui-CustomFileInput-input").addValue("/Users/k001310/Documents/SD/aspire/aspire-automation-framework/resource/indentifyCard.jpg")
-        // browser.pause(3000);
-        // $(".onfido-sdk-ui-Button-button-primary").click();
-        // browser.pause(5000);
-        // $(".onfido-sdk-ui-CustomFileInput-input").addValue("/Users/k001310/Documents/SD/aspire/aspire-automation-framework/resource/indentifyCard.jpg")
-        // browser.pause(3000);
-        // $(".onfido-sdk-ui-Button-button-primary").click();
-        // browser.pause(5000);
-        // $(".onfido-sdk-ui-Button-button-primary").click();
-        // browser.pause(5000);
-        // $(".onfido-sdk-ui-Camera-btn").click();
-        // // $(".onfido-sdk-ui-Button-button-primary").click();
-        // // browser.pause(5000);
-        // // browser.keys("Escape");
-        // browser.pause(10000);
+        page.IdentifyDetails.addIdentify();
     });
 });
