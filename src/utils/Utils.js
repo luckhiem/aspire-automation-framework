014b875e987dfd376cdd7a4553ca8daa171a292c
KhiemLuc <41050460+luckhiem@users.noreply.github.com>
2020-07-28 12:21:40 +0700

    update

--- src/utils/Utils.js
+++ src/utils/Utils.js
@@ -1,6 +1,4 @@
 
-const User = require('../entity/User');
-
 class Utils {
     generateNumber() {
         return Math.floor(10000000 + Math.random() * 90000000)
@@ -8,19 +6,6 @@ class Utils {
 
     generateUEN() {
         return Math.floor(100000000 + Math.random() * 900000000) + "M"
-    }
-
-    generateUsers(number, data) {
-        const result = []
-        for (let i = 0; i < number; i++) {
-            const user = new User({});
-            user.firstName = data.firstName;
-            user.lastName = data.lastName;
-            user.email = data.lastName + data.firstName + i + '@gmail.com';
-            user.manager = data.manager;
-            result.push(user);
-        }
-        return result;
     }
 };
 
