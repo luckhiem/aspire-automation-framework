ceeaf9f4dd912b269585b734b0370337da0b306d
Luc Tran Vinh Khiem <k001310@PC-001310-4.local>
2020-07-28 08:51:23 +0700

    update

--- src/pages/identify_details/IdentifyDetails.js
+++ src/pages/identify_details/IdentifyDetails.js
@@ -7,12 +7,14 @@ const path = require('path');
 const filePath = path.join(__dirname, '../../../resource/identifyCard.jpg');
 
 const IDENTIFY_DETAIL = '.new-form__view';
-const SUBMIT_BTN = "div[label='Continue'] button";
+const CONTINUE_BTN = "div[label='Continue'] button";
 const BEGIN_BTN = "div[label='Begin Verification'] button";
 const IDENTIFY_CARD_BTN = '.onfido-sdk-ui-DocumentSelector-icon-national-identity-card';
 const IDENTIFY_CARD_UPLOAD = '.onfido-sdk-ui-CustomFileInput-input';
 const CARD_SUBMIT_BTN = '.onfido-sdk-ui-Button-button-primary';
 const TAKE_SELFIE_BTN = '.onfido-sdk-ui-Camera-btn';
+const NRIC_NUMBER_TXB = 'input[data-cy="identity-edit-nric"]';
+const SUBMIT_BTN = ".aspire-button--cta"
 
 class IdentifyDetails {
     acccessIdentifyDetailPage() {
@@ -23,13 +25,13 @@ class IdentifyDetails {
     }
 
     clickSubmitBtn() {
-        ElementHandler.waitForElementDisplayed(SUBMIT_BTN)
-        ElementHandler.click(SUBMIT_BTN);
+        ElementHandler.waitForElementDisplayed(CONTINUE_BTN)
+        ElementHandler.click(CONTINUE_BTN);
         return this;
     }
 
     beginVerification() {
-        ElementHandler.waitForElementDisplayed(SUBMIT_BTN)
+        ElementHandler.waitForElementDisplayed(BEGIN_BTN)
         ElementHandler.click(BEGIN_BTN);
         return this;
     }
@@ -50,19 +52,27 @@ class IdentifyDetails {
         browser.pause(10000)
         $(TAKE_SELFIE_BTN).click();
         ElementHandler.click(CARD_SUBMIT_BTN);
+        Common.waitForPageLoading();
+        return this;
+    }
+
+    inputNRICNumber(number) {
+        ElementHandler.addValue(NRIC_NUMBER_TXB, number);
+        ElementHandler.click(SUBMIT_BTN)
         return this;
     }
 
     /**
-     * @param {Business} business
+     * @param {User} user
      */
-    addIdentify() {
+    addIdentify(user) {
         this.acccessIdentifyDetailPage();
         this.clickSubmitBtn();
         this.beginVerification();
         this.verifyIdentifyCard();
         this.takeSelfie();
-        browser.pause(10000)
+        this.inputNRICNumber(user.number)
+        Common.waitForPageLoading();
         return this;
     }
 }
